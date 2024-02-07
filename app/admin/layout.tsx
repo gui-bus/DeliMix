651566ced9d5288fe4ed14d277dcf55402615dd0
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-07 19:34:34 -0300

    feat: add initial admin page

---
+++ app/admin/layout.tsx
@@ -0,0 +1,13 @@
+import Footer from "@/components/common/footer";
+import Header from "@/components/common/header";
+import { ReactNode } from "react";
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <Header />
+      <div className="flex-grow">{children}</div>
+      <Footer />
+    </div>
+  );
+}
---
+++ app/admin/page.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const Admin = () => {
+  return (
+    <section className="mx-auto flex w-full max-w-7xl flex-col">
+      <div className="p-5">
+        <h1>Admin page</h1>
+
+        <div className="flex items-center gap-5">
+          <Button
+            variant={"default"}
+            size={"lg"}
+            className="hidden rounded-3xl md:flex"
+            asChild
+          >
+            <Link
+              href="/admin/new/category"
+              className="flex w-full items-center justify-center gap-2"
+            >
+              Nova categoria
+            </Link>
+          </Button>
+
+          <Button
+            variant={"default"}
+            size={"lg"}
+            className="hidden rounded-3xl md:flex"
+            asChild
+          >
+            <Link
+              href="/admin/new/product"
+              className="flex w-full items-center justify-center gap-2"
+            >
+              Novo produto
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Admin;
