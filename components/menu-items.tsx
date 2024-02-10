eb006bfe97d7b36754754e8738091644aa2ac811
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-10 14:02:53 -0300

    feat: add contact button

--- components/menu-items.tsx
+++ components/menu-items.tsx
@@ -1,6 +1,25 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { prismaClient } from "@/lib/prisma";
 import ProductsSection from "@/sections/products-section";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { FaWhatsapp } from "react-icons/fa6";
+
+const tel = 12981847553;
+var text =
+  "*Seja muito bem-vindo ao WhatsApp da DeliMix! Estamos aqui para atendê-lo com todo o prazer.*\n";
+text +=
+  "Para realizar seu pedido, precisamos de algumas informações. Caso tenha alguma dúvida, fique à vontade para perguntar!\n\n";
+text += "*Seu nome:*\n";
+text += "*O que você gostaria de pedir hoje?*\n";
+text += "*Endereço de entrega e ponto de referência:*\n";
+text +=
+  "*Qual será a forma de pagamento. (Dinheiro, PIX, Crédito, Débito, VA/VR)*\n\n";
+
+text += "*Obrigado por escolher a Delimix!*";
+
+let encode = encodeURIComponent(text);
+let URL = `https://wa.me/${tel}?text=${encode}`;
 
 const MenuItems = async () => {
   const categories = await prismaClient.category.findMany({});
@@ -9,7 +28,7 @@ const MenuItems = async () => {
     <div className="my-8 flex flex-col items-center justify-center gap-5">
       <div className="flex flex-col items-center justify-center px-5">
         <span className="font-light text-primary">CARDÁPIO</span>
-        <h2 className="text-2xl font-black italic uppercase text-center">
+        <h2 className="text-center text-2xl font-black uppercase italic">
           Explore nossa seleção de sabores únicos
         </h2>
 
@@ -17,6 +36,12 @@ const MenuItems = async () => {
           Desde clássicos irresistíveis até novidades limitadas, temos algo para
           satisfazer todos os paladares
         </p>
+
+        <Button variant={"outline"} asChild className="rounded-xl mt-5 w-full max-w-md mx-auto">
+          <Link href={URL} target="_blank" className="flex items-center gap-2">
+            Faça seu pedido <FaWhatsapp size={20} />
+          </Link>
+        </Button>
       </div>
 
       <Tabs
