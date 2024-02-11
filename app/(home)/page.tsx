f30cbf0e43712e4b45f64b7634d2632410b9d4c8
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-10 23:39:58 -0300

    feat: add buttons to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,21 +1,59 @@
 import MenuItems from "@/components/menu-items";
+import { Button } from "@/components/ui/button";
 import AboutSection from "@/sections/about-section";
 import WelcomeSection from "@/sections/welcome-section";
 import Image from "next/image";
+import Link from "next/link";
+import {
+  FaInstagram,
+  FaXTwitter,
+  FaLinkedinIn,
+  FaWhatsapp,
+} from "react-icons/fa6";
 
 export default function Home() {
   return (
     <section>
-      <Image
-        src="/banner.png"
-        alt="DeliMix Banner"
-        width={0}
-        height={0}
-        className="h-auto w-full object-cover"
-        sizes="100vw"
-        priority
-        draggable="false"
-      />
+      <div className="relative">
+        <Image
+          src="/banner.png"
+          alt="DeliMix Banner"
+          width={0}
+          height={0}
+          className="h-auto w-full object-cover"
+          sizes="100vw"
+          priority
+          draggable="false"
+        />
+
+        <div className="absolute bottom-3 right-3 hidden md:block">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Button size={"icon"} variant={'outline'} asChild>
+              <Link href={"#"}>
+                <FaInstagram size={20} />
+              </Link>
+            </Button>
+
+            <Button size={"icon"} variant={'outline'} asChild>
+              <Link href={"#"}>
+                <FaXTwitter size={20} />
+              </Link>
+            </Button>
+
+             <Button size={"icon"} variant={'outline'} asChild>
+              <Link href={"#"}>
+                <FaLinkedinIn size={20} />
+              </Link>
+            </Button>
+
+            <Button size={"icon"} variant={'outline'} asChild>
+              <Link href={"#"}>
+                <FaWhatsapp size={20} />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
       <div className="mx-auto flex w-full max-w-6xl flex-col">
         <WelcomeSection />
 
